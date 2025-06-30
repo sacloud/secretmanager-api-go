2b5b486be262839942f41dc8cebdf013bd7da23d
Masahiro Nakagawa <repeatedly@gmail.com>
2025-06-30 10:52:23 +0900

    Migrate from example_test to keys_test.go
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

---
+++ secrets_test.go
@@ -0,0 +1,81 @@
+package secretmanager_test
+
+import (
+	"context"
+	"os"
+	"sort"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/sacloud/packages-go/testutil"
+	sm "github.com/sacloud/secretmanager-api-go"
+	v1 "github.com/sacloud/secretmanager-api-go/apis/v1"
+)
+
+func TestSecretAPI(t *testing.T) {
+	testutil.PreCheckEnvsFunc("SAKURACLOUD_ACCESS_TOKEN",
+		"SAKURACLOUD_ACCESS_TOKEN_SECRET", "SAKURACLOUD_KMS_KEY_ID")(t)
+
+	client, err := sm.NewClient()
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	keyId := os.Getenv("SAKURACLOUD_KMS_KEY_ID")
+	vaultOp := sm.NewVaultOp(client)
+
+	vault, err := vaultOp.Create(ctx, v1.CreateVault{
+		Name:        "vault for secret test",
+		Description: v1.NewOptString("vault for secret test"),
+		KmsKeyID:    keyId,
+		Tags:        []string{"Test"},
+	})
+	require.NoError(t, err)
+
+	defer func() {
+		_ = vaultOp.Delete(ctx, vault.ID)
+	}()
+
+	secOp := sm.NewSecretOp(client, vault.ID)
+
+	for i := 0; i < 2; i++ {
+		resCreate, err := secOp.Create(ctx, v1.CreateSecret{
+			Name:  "Sec1",
+			Value: "SecretValue" + strconv.Itoa(i),
+		})
+		require.NoError(t, err)
+		require.Equal(t, i+1, resCreate.LatestVersion)
+	}
+	resCreate, err := secOp.Create(ctx, v1.CreateSecret{
+		Name:  "Sec2",
+		Value: "SV22",
+	})
+	require.NoError(t, err)
+	assert.Equal(t, 1, resCreate.LatestVersion)
+
+	resList, err := secOp.List(ctx)
+	assert.NoError(t, err)
+
+	sort.Slice(resList, func(i, j int) bool { return resList[i].Name < resList[j].Name })
+	require.Len(t, resList, 2)
+	assert.Equal(t, "Sec1", resList[0].Name)
+	assert.Equal(t, 2, resList[0].LatestVersion)
+	assert.Equal(t, "Sec2", resList[1].Name)
+	assert.Equal(t, 1, resList[1].LatestVersion)
+
+	for i := 0; i < 2; i++ {
+		resUn, err := secOp.Unveil(ctx, v1.Unveil{
+			Name:    "Sec1",
+			Version: v1.NewOptNilInt(i + 1),
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, "SecretValue"+strconv.Itoa(i), resUn.Value)
+	}
+
+	err = secOp.Delete(ctx, v1.DeleteSecret{Name: "Sec1"})
+	require.NoError(t, err)
+	err = secOp.Delete(ctx, v1.DeleteSecret{Name: "Sec2"})
+	require.NoError(t, err)
+}
---
+++ vaults_test.go
@@ -0,0 +1,65 @@
+package secretmanager_test
+
+import (
+	"context"
+	"os"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/sacloud/packages-go/testutil"
+	sm "github.com/sacloud/secretmanager-api-go"
+	v1 "github.com/sacloud/secretmanager-api-go/apis/v1"
+)
+
+func TestVaultAPI(t *testing.T) {
+	testutil.PreCheckEnvsFunc("SAKURACLOUD_ACCESS_TOKEN",
+		"SAKURACLOUD_ACCESS_TOKEN_SECRET", "SAKURACLOUD_KMS_KEY_ID")(t)
+
+	client, err := sm.NewClient()
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	keyId := os.Getenv("SAKURACLOUD_KMS_KEY_ID")
+	vaultOp := sm.NewVaultOp(client)
+
+	resCreate, err := vaultOp.Create(ctx, v1.CreateVault{
+		Name:        "vault from go",
+		Description: v1.NewOptString("vault from go client"),
+		KmsKeyID:    keyId,
+		Tags:        []string{"App", "Vault"},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, "vault from go", resCreate.Name)
+
+	resList, err := vaultOp.List(ctx)
+	assert.NoError(t, err)
+
+	sort.Slice(resList, func(i, j int) bool { return resList[i].ID < resList[j].ID })
+	found := false
+	for _, vault := range resList {
+		if vault.ID == resCreate.ID {
+			require.Equal(t, "vault from go client", vault.Description.Value)
+			found = true
+		}
+	}
+	assert.True(t, found, "created vault not found in list")
+
+	_, err = vaultOp.Update(ctx, resCreate.ID, v1.Vault{
+		Name:        "vault from go 2",
+		Description: v1.NewOptString("vault from go client 2"),
+		KmsKeyID:    keyId,
+		Tags:        []string{"Test"},
+	})
+	assert.NoError(t, err)
+
+	resRead, err := vaultOp.Read(ctx, resCreate.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, "vault from go 2", resRead.Name)
+	assert.Equal(t, "vault from go client 2", resRead.Description.Value)
+
+	err = vaultOp.Delete(ctx, resCreate.ID)
+	require.NoError(t, err)
+}
